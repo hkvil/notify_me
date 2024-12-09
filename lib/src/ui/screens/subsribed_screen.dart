2a9f49e5dc0eeb76beb75281c4bcd3bffc916d51
Hidayat <75840967+hkvil@users.noreply.github.com>
2024-12-09 14:29:20 +0700

    Finished Subsribed Screen

--- lib/src/ui/screens/subsribed_screen.dart
+++ lib/src/ui/screens/subsribed_screen.dart
@@ -1,12 +1,22 @@
+//Nanti tambah search bar juga sama filter by recent read
+
 import 'package:flutter/material.dart';
+import 'package:notify_me/src/style.dart';
+import 'package:notify_me/src/ui/widgets/list_view_comic.dart';
 
 class SubsribedScreen extends StatelessWidget {
   const SubsribedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Subsribed Page!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Subscribed Comics',
+          style: titleTextStyle,
+        ),
+      ),
+      body: ListViewComic(),
     );
   }
 }
---
+++ lib/src/ui/widgets/list_view_comic.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:notify_me/src/data/comic.dart';
+import 'package:notify_me/src/style.dart';
+import 'package:notify_me/src/utils.dart';
+
+class ListViewComic extends StatefulWidget {
+  const ListViewComic({
+    super.key,
+  });
+
+  @override
+  State<ListViewComic> createState() => _ListViewComicState();
+}
+
+class _ListViewComicState extends State<ListViewComic> {
+  List<Comic> comics = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchComics();
+  }
+
+  Future<void> fetchComics() async {
+    final fetchedComics = await Comic.fetchComics();
+    setState(() {
+      comics = fetchedComics;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: ListView.builder(
+        itemCount: comics.length,
+        itemBuilder: (context, index) {
+          final comic = comics[index];
+          return ListTile(
+            contentPadding: const EdgeInsets.all(8.0),
+            leading: Image.asset(
+              comic.imageUrl,
+              semanticLabel: comic.title,
+              fit: BoxFit.cover,
+              width: 50,
+              height: 50,
+            ),
+            title: Text(
+              comic.title,
+              style: titleTextStyle,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            trailing: Text('${comic.readChapter}/${comic.lastChapter}'),
+            onTap: () => navigateToComicScreen(context, comic),
+          );
+        },
+      ),
+    );
+  }
+}
